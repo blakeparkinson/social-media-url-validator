23a8e5ef0835c47691d36cd90dc23232e4669e0b
Blake Parkinson <wizardplow@gmail.com>
2021-09-17 13:45:21 -0600

    Adds more regex

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 exports.validate = (platform, socialMediaUrl) =>  {
     platform = platform.toLowerCase()
     const regexMap = {
-        facebook:
+        facebookUrl:
 		/^(https?:\/\/)?(?:www\.)?(?:facebook|fb|m\.facebook)\.(?:com|me)\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[\w-]*\/)*([\w\-.]+)(?:\/)?/i,
-	    instagram: /(https?)?:?(www)?instagram\.com/,
-	    twitter:
-		/(?:http:\/\/)?(?:www\.)?twitter\.com\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[\w-]*\/)*([\w-]*)/,
-	    linkedin:
-		/^(http(s)?:\/\/)?([\w]+\.)?linkedin\.com\/(pub|in|profile|company)/,
+	    instagramUrl: /http(?:s)?:\/\/(?:www\.)?instagram\.com\/([a-zA-Z0-9_]+)/,
+	    twitterUrl: /http(?:s)?:\/\/(?:www\.)?twitter\.com\/([a-zA-Z0-9_]+)/,
+
+	    linkedinUrl:
+		/^(http(s)?:\/\/)?([\w]+\.)?linkedin\.com\/(pub|in|profile|company)\/([a-zA-Z0-9_]+)/,
 	    emailaddress: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
     }
     let isValid = false
