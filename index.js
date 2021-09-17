fbd208f498760475e238589f9cd6f75d87d1d39b
Blake Parkinson <wizardplow@gmail.com>
2021-09-17 16:46:15 -0600

    fixes

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 exports.validate = (platform, socialMediaUrl) =>  {
     platform = platform.toLowerCase()
     const regexMap = {
-        facebookUrl:
+        facebook:
 		/^(https?:\/\/)?(?:www\.)?(?:facebook|fb|m\.facebook)\.(?:com|me)\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[\w-]*\/)*([\w\-.]+)(?:\/)?/i,
-	    instagramUrl: /http(?:s)?:\/\/(?:www\.)?instagram\.com\/([a-zA-Z0-9_]+)/,
-	    twitterUrl: /http(?:s)?:\/\/(?:www\.)?twitter\.com\/([a-zA-Z0-9_]+)/,
+	    instagram: /http(?:s)?:\/\/(?:www\.)?instagram\.com\/([a-zA-Z0-9_]+)/,
+	    twitter: /http(?:s)?:\/\/(?:www\.)?twitter\.com\/([a-zA-Z0-9_]+)/,
 
-	    linkedinUrl:
+	    linkedin:
 		/^(http(s)?:\/\/)?([\w]+\.)?linkedin\.com\/(pub|in|profile|company)\/([a-zA-Z0-9_]+)/,
 	    emailaddress: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
     }
