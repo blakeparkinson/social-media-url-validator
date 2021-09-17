271bbd0015c10a439da4d307df6283ff3e3099ee
Blake Parkinson <wizardplow@gmail.com>
2021-09-17 13:37:26 -0600

    Adding facebook update to regex

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.validate = (platform, socialMediaUrl) =>  {
     platform = platform.toLowerCase()
     const regexMap = {
         facebook:
-		/(?:https?:\/\/)?(?:www\.)?(?:facebook|fb|m\.facebook)\.(?:com|me)\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[\w-]*\/)*([\w\-.]+)(?:\/)?/i,
+		/^(https?:\/\/)?(?:www\.)?(?:facebook|fb|m\.facebook)\.(?:com|me)\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[\w-]*\/)*([\w\-.]+)(?:\/)?/i,
 	    instagram: /(https?)?:?(www)?instagram\.com/,
 	    twitter:
 		/(?:http:\/\/)?(?:www\.)?twitter\.com\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[\w-]*\/)*([\w-]*)/,
